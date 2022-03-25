7b7a2dd6fdc8e1065b0bc5875c6ac50894aa91e3
Naja-Naja <latimeria.h11@gmail.com>
2022-03-25 12:59:08 +0900

    firstcommit

---
+++ Assets/Project/AvatarNameDisplay.cs
@@ -0,0 +1,13 @@
+using Photon.Pun;
+using UnityEngine;
+
+// MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
+public class AvatarNameDisplay : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        var nameLabel = GetComponent<TextMesh>();
+        // プレイヤー名とプレイヤーIDを表示する
+        nameLabel.text = $"{photonView.Owner.NickName}({photonView.OwnerActorNr})";
+    }
+}
---
+++ Assets/Project/SampleScene.cs
@@ -0,0 +1,32 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+// MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
+public class SampleScene : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        // プレイヤー自身の名前を"Player"に設定する
+        PhotonNetwork.NickName = "Player";
+        // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster()
+    {
+        // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom()
+    {
+        
+        // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
+        var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
+        
+    }
+}
