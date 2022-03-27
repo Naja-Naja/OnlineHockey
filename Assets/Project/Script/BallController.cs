569b72b68d696b9e3f4c0cc5e9f768c84d7f3d30
Naja-Naja <latimeria.h11@gmail.com>
2022-03-27 20:29:27 +0900

    各種不具合を修正

--- Assets/Project/Script/BallController.cs
+++ Assets/Project/Script/BallController.cs
@@ -8,27 +8,41 @@ public class BallController : MonoBehaviourPunCallbacks
     [SerializeField] Rigidbody2D rb2d;
     bool movestart = false;
     bool IsGameEnd = false;
-    // Start is called before the first frame update
+    bool speedCon = false;
     void Start()
     {
         Invoke("ballStart", 3f);
 
     }
+
     private void FixedUpdate()
     {
+        if (speedCon == false) { return; }
         var speed = rb2d.velocity;
-        if (Mathf.Abs(speed.x) < 0.1&&movestart==true&&IsGameEnd==false)
+        if (Mathf.Abs(speed.x) < 0.1 && movestart == true && IsGameEnd == false)
         {
-            rb2d.AddForce(new Vector2(Mathf.Sign(speed.x)*15f, 0f));
+            rb2d.AddForce(new Vector2(Mathf.Sign(speed.x) * 15f, 0f));
+            //Debug.Log(Mathf.Sign(speed.x) * 15);
+        }
+        if (Mathf.Abs(speed.y) < 0.1 && movestart == true && IsGameEnd == false)
+        {
+            rb2d.AddForce(new Vector2(0f, Mathf.Sign(speed.y) * 5f));
             //Debug.Log(Mathf.Sign(speed.x) * 15);
         }
     }
+
     void ballStart()
     {
         rb2d.AddForce(new Vector2(15f, 15f));
         //rb2d.AddForce(new Vector2(-30f, -15f));//test
         movestart = true;
+        Invoke("startSpeedCon", 0.5f);
     }
+    void startSpeedCon()
+    {
+        speedCon = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -53,17 +67,17 @@ public class BallController : MonoBehaviourPunCallbacks
         rb2d.velocity = rb2d.velocity * 1.02f;
         
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "rpcline")
-        {
-            Debug.Log("Dead");
-            photonView.RPC(nameof(RpcBallDead), RpcTarget.All,
-                 new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z),
-                 rb2d.velocity
-    );
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "rpcline")
+    //    {
+    //        Debug.Log("Dead");
+    //        photonView.RPC(nameof(RpcBallDead), RpcTarget.All,
+    //             new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z),
+    //             rb2d.velocity
+    //);
+    //    }
+    //}
 
     [PunRPC]
     private void RpcBallBound(Vector3 position,Vector2 vector)
@@ -71,11 +85,11 @@ public class BallController : MonoBehaviourPunCallbacks
         this.transform.position = position;
         rb2d.velocity = vector*1.02f;
     }
-    [PunRPC]
-    private void RpcBallDead(Vector3 position, Vector2 vector)
-    {
-        this.transform.position = position;
-        rb2d.velocity = vector;
-    }
+    //[PunRPC]
+    //private void RpcBallDead(Vector3 position, Vector2 vector)
+    //{
+    //    this.transform.position = position;
+    //    rb2d.velocity = vector;
+    //}
 
 }
--- Assets/Project/Script/Manager.cs
+++ Assets/Project/Script/Manager.cs
@@ -5,58 +5,45 @@ using Photon.Pun;
 using Photon.Realtime;
 using UniRx;
 using System;
-
+using ExitGames.Client.Photon;
 public class Manager : MonoBehaviourPunCallbacks
 {
     public ReactiveProperty<String> result;
     public ReactiveProperty<bool> canRetry;
-    // Start is called before the first frame update
+    private String team = "null";
+
     void Awake()
     {
         canRetry.Value = false;
     }
+
     private void Start()
     {
+        //retryボタンでgameInit(false)を呼ぶ
         IDisposable subscription = canRetry.Subscribe(x => {
             gameInit(x);
         });
     }
+
     public override void OnJoinedRoom()
     {
         if (PhotonNetwork.IsMasterClient)
         {
             Debug.Log("自身がマスタークライアントです");
+            team = "Left";
             GameObject.Find("ownerDeadline").GetComponent<DeadLine>().GameOver.Subscribe(x => loseEvent());
-            GameObject.Find("guestDeadline").GetComponent<DeadLine>().GameOver.Subscribe(x => winEvent());
+            //GameObject.Find("guestDeadline").GetComponent<DeadLine>().GameOver.Subscribe(x => winEvent());
 
         }
         else
         {
             Debug.Log("マスタークライアントではありません");
-            GameObject.Find("ownerDeadline").GetComponent<DeadLine>().GameOver.Subscribe(x => winEvent());
+            team = "Right";
+            //GameObject.Find("ownerDeadline").GetComponent<DeadLine>().GameOver.Subscribe(x => winEvent());
             GameObject.Find("guestDeadline").GetComponent<DeadLine>().GameOver.Subscribe(x => loseEvent());
         }
     }
-    void winEvent()
-    {
-        Debug.Log("勝ち");
-        result.Value = "Win";
-        gameEndEvent();
-    }
-    void loseEvent()
-    {
-        Debug.Log("負け");
-        result.Value = "Lose";
-        gameEndEvent();
-    }
-    void gameEndEvent()
-    {
-        //共通処理部分
-        if (PhotonNetwork.IsMasterClient)
-        {
-            canRetry.Value = true;
-        }
-    }
+    
     void gameInit(bool retry)
     {
         Debug.Log("InitCall");
@@ -72,15 +59,70 @@ public class Manager : MonoBehaviourPunCallbacks
             {
                 tmp[i].GetComponent<AvatarManager>().gameStart = false;
             }
-
-
         }
     }
+
+    //負けると発火して部屋のプロパティを設定
+    void loseEvent()
+    {
+        Debug.Log("負け");
+        result.Value = "Lose";
+        //photonView.RPC(nameof(RpcGameInit), RpcTarget.All);
+        var hashtable = new ExitGames.Client.Photon.Hashtable();
+        if (team == "Right")
+        {
+            hashtable["Winner"] = "Left";
+        }
+        else if (team == "Left")
+        {
+            hashtable["Winner"] = "Right";
+        }
+        PhotonNetwork.CurrentRoom.SetCustomProperties(hashtable);
+    }
+
+    //部屋のプロパティが変更されると自身を勝者か判定する
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        // 更新されたルームのカスタムプロパティのペアをコンソールに出力する
+        //foreach (var prop in propertiesThatChanged)
+        //{
+        //    Debug.Log($"{prop.Key}: {prop.Value}");
+        //}
+        string winner = (PhotonNetwork.CurrentRoom.CustomProperties["Winner"] is string value) ? value : "null";
+        if (winner == team)
+        {
+            winEvent();
+        }
+        //ボールを削除
+        var ball = GameObject.FindWithTag("ball");
+        Destroy(ball);
+        gameEndEvent();
+    }
+
+    void winEvent()
+    {
+        Debug.Log("勝ち");
+        result.Value = "Win";
+    }
+
+    //マスタークライアントは再戦ボタンを出現
+    void gameEndEvent()
+    {        
+        //photonView.RPC(nameof(RpcGameInit), RpcTarget.All);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            canRetry.Value = true;
+        }
+    }
+
     [PunRPC]
     void RpcGameInit()
     {
         result.Value = "";
-        var ball = GameObject.FindWithTag("ball");
-        Destroy(ball);
     }
+
+
+
+
+
 }
