166aff89d6b35279e33cb85d8f0246ac681344ee
Naja-Naja <latimeria.h11@gmail.com>
2022-03-31 23:46:02 +0900

    BGMを実装

--- Assets/Project/Script/AudioManager.cs
+++ Assets/Project/Script/AudioManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class AudioManager : MonoBehaviour
 {
@@ -8,11 +9,16 @@ public class AudioManager : MonoBehaviour
     [SerializeField] public AudioSource BGM = null;
     static AudioSource SE_audio = null;
     static AudioSource BGM_audio = null;
+    public float bgmVol;
+    static Sequence seq;
     //[SerializeField] AudioClip audioclip = null;
     // Start is called before the first frame update
     void Start()
     {
         SE_audio = SE;
+        BGM_audio = BGM;
+        //DOTween.To(() => BGM_audio.volume, (x) => BGM_audio.volume = x, 0f, 0.5f);
+
     }
     public static void SE_Play(AudioClip clip)
     {
@@ -21,7 +27,34 @@ public class AudioManager : MonoBehaviour
     }
     public static void BGM_Play(AudioClip clip)
     {
+        //if (seq == null)
+        //{
+        //    var tweenFadeOut = DOTween.To(() => BGM_audio.volume, x => BGM_audio.volume = x, 0f, 3f);
+        //    var tweenFadeIn = DOTween.To(() => BGM_audio.volume, x => BGM_audio.volume = x, 0.5f, 3f).OnStart(() => changeBGM(clip));
+        //    seq = DOTween.Sequence().Append(tweenFadeOut).Append(tweenFadeIn);
+        //}
+        //else
+        if (seq!=null&&DOTween.IsTweening(seq))
+        {
+            // Š®—¹‚µ‚Ä‚¢‚È‚¢‚½‚ßRestart‚·‚é
+            seq.Restart();
+            Debug.Log("restart");
+        }
+        else
+        {
+            var tweenFadeOut = DOTween.To(() => BGM_audio.volume, x => BGM_audio.volume = x, 0f, 1f);
+            var tweenFadeIn = DOTween.To(() => BGM_audio.volume, x => BGM_audio.volume = x, 0.5f, 1f).OnStart(() => changeBGM(clip));
+            seq = DOTween.Sequence().Append(tweenFadeOut).Append(tweenFadeIn);
+        }
+
+
+        //seq.Append(tweenFadeOut);
+        //seq.Append(tweenFadeIn);
+        Debug.Log("playBGM");
+    }
+    private static void changeBGM(AudioClip clip)
+    {
         BGM_audio.clip = clip;
-        SE_audio.Play();
+        BGM_audio.Play();
     }
 }
---
+++ Assets/Project/Script/BGMChanger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BGMChanger : MonoBehaviourPunCallbacks
+{
+    [SerializeField] AudioClip gameBGM;
+    [SerializeField] AudioClip roomBGM;
+    AudioClip nowBGM;
+    //private void Start()
+    //{
+    //    AudioManager.BGM_Play(roomBGM);
+    //    nowBGM = roomBGM;
+    //}
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        string room = (string)PhotonNetwork.CurrentRoom.CustomProperties["RoomState"];
+        if (room == "Game" || room == "Result")
+        {
+            if (nowBGM != gameBGM)
+            {
+                AudioManager.BGM_Play(gameBGM);
+                nowBGM = gameBGM;
+            }
+
+        }
+        else if(nowBGM!=roomBGM)
+        {
+            AudioManager.BGM_Play(roomBGM);
+            nowBGM = roomBGM;
+        }
+    }
+}
